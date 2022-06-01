440ed10c5824d19e2e972491da3f1c5781cab42c
Westin Humble <westin.h@gmail.com>
2022-06-01 17:34:14 -0400

    solved leetcode-70

--- leetcode-70.js
+++ leetcode-70.js
@@ -25,6 +25,25 @@
 
 // 1 <= n <= 45
 
-let climbStairs = function(n) {
+
+const climbStairs = function(n) {
+    // create conditional for returning n if it is less than or equal to 2 (since it won't be bigger than n if n is 1 or 2)
+    // declare variables that denote all possible step combinations and counter variable
+    // if n is bigger than 2, loop over number and after each iteration update counter variable 
+    // set prev2 equal to one 
+    // set prev1 equal to counter
+    // return counter
+    if(n <= 2) return n;
     
+    let prev2 = 1;
+    let prev1 = 2;
+    let curr = 0;
+    for(let i = 3; i <= n; i++) {
+        curr = prev2 + prev1;
+        prev2 = prev1;
+        prev1 = curr;
+    }
+    console.log(curr);
 };
+
+console.log(climbStairs(5))
