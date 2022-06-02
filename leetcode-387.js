bbe758df36bcf85f461d63cf980974eab95d9b29
Westin Humble <westin.h@gmail.com>
2022-06-02 12:37:52 -0400

    leetcode-387 solved

--- leetcode-387.js
+++ leetcode-387.js
@@ -16,5 +16,17 @@
 // Output: -1
 
 const firstUniqChar = function(s) {
-    
+    // iterate through string using for loop
+    // conditional using indexOf and lastIndexOf to check for duplicating characters
+    // return index for first repeating character
+    // if not present, return -1
+
+    for(i=0;i<s.length;i++){
+        if (s.indexOf(s[i])===s.lastIndexOf(s[i])){
+           return i;
+       } 
+    }
+    return -1;
 };
+
+console.log(firstUniqChar('loveleetcode'));
