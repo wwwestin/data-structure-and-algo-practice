4054b53c1c4d5f4832104e88936b5c359152bd82
Westin Humble <westin.h@gmail.com>
2022-06-03 13:11:02 -0400

    leetcode 9 solved

--- leetcode-9.js
+++ leetcode-9.js
@@ -4,3 +4,21 @@
 
 // For example, 121 is a palindrome while 123 is not.
 
+var isPalindrome = function(x) {
+    //convert x to a string
+    //convert string to array
+    //reverse array
+    //join and return to string
+    //return boolean if reversed string is strictly equal to x as string
+    //tools: toString, .split, array.reverse, .join
+    
+    let convert = x.toString();
+    let arr = convert.split("");
+    let rev = arr.reverse();
+    let j = rev.join('');
+    
+    return convert === j 
+    
+};
+
+console.log(isPalindrome("121"))
