dea081c059fc442f9d2f72cfad3906b2e1d5de18
Westin Humble <westin.h@gmail.com>
2022-06-07 11:42:14 -0500

    leetcode 1662 solved

--- leetcode-1662.js
+++ leetcode-1662.js
@@ -13,6 +13,23 @@
 // word2 represents string "a" + "bc" -> "abc"
 // The strings are the same, so return true.
 
+const word1 = ["ab", "c"]
+const word2 = ["a", "bc"]
+
 const arrayStringsAreEqual = function(word1, word2) {
+    // declare variables to join strings
+    // conditional to check if two join words are strictly equal
+    // return boolean depending on congruency
     
+    let w1=word1.join("")
+    let w2=word2.join("")
+
+    if(w1===w2)
+        return true;
+    return false
+
 };
+
+console.log(arrayStringsAreEqual(word1, word2))
+
+
