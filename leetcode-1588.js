3773476875c447b022957ea941bd117d5964001b
Westin Humble <westin.h@gmail.com>
2022-06-13 10:53:18 -0400

    leetcode 1588 solved

--- leetcode-1588.js
+++ leetcode-1588.js
@@ -21,5 +21,26 @@
 // If we add all these together we get 1 + 4 + 2 + 5 + 3 + 7 + 11 + 10 + 15 = 58
 
 const sumOddLengthSubarrays = function(arr) {
+// declare counter variable
+// outer for loop to determine subarry start point
+// nested for loop to determine subarray end point
+// conditional to ensure that only odd length subarrays are counted
+// another nested for loop to iterate array from start to finish 
+// count increases for each iteration of k
+// return count
+
+    let count=0;
     
+    for(let i = 0; i < arr.length; i++){ 
+        for(let j = i; j < arr.length; j++){  
+            if((i - j) %2 ==0){ 
+				for(let k = i; k <= j; k++){ 
+					count+=arr[k];
+				}
+            }
+        }
+    }
+    return count;
 };
+
+console.log(sumOddLengthSubarrays([1,4,2,5,3]));
