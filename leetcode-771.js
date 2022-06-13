a5489a4c98db3a4e5d87354de5307fd45e6de7ff
Westin Humble <westin.h@gmail.com>
2022-06-13 11:42:23 -0400

    leetcode 771 repeat

--- leetcode-771.js
+++ leetcode-771.js
@@ -8,6 +8,7 @@
 
 // Input: jewels = "aA", stones = "aAAbbbb"
 // Output: 3
+
 // Example 2:
 
 // Input: jewels = "z", stones = "ZZ"
@@ -16,8 +17,8 @@
 const numJewelsInStones = function(jewels, stones) {
     // declare counter variable
     // delcare conditional to return 0 if neither jewels nor stones are in parameters
-    // for of loop to loop through stones to determine if jewels are present insode of stones
-    // if present inside of stone, count is incremented by 1
+    // for-of loop to iterate through stones to determine if jewels are present inside of stones
+    // if jewels are present inside of stone, count is incremented by 1
     // return count once all iterables are looped over
     
     if (!jewels || !stones) return 0;
@@ -31,4 +32,5 @@ const numJewelsInStones = function(jewels, stones) {
     return count;
 };
 
-console.log(numJewelsInStones("j", "jJJJjJJJj"))
+console.log(numJewelsInStones("j", "jJJJjJJJj"));
+console.log(numJewelsInStones("z", "ZZ"));
