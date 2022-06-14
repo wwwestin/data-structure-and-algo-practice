69897449035d37fffa81e1a6a25ccfb54f97825c
Westin Humble <westin.h@gmail.com>
2022-06-14 15:24:35 -0400

    leetcode 9 repeat

--- leetcode-9.js
+++ leetcode-9.js
@@ -5,20 +5,16 @@
 // For example, 121 is a palindrome while 123 is not.
 
 var isPalindrome = function(x) {
-    //convert x to a string
-    //convert string to array
-    //reverse array
-    //join and return to string
-    //return boolean if reversed string is strictly equal to x as string
-    //tools: toString, .split, array.reverse, .join
-    
-    let convert = x.toString();
-    let arr = convert.split("");
-    let rev = arr.reverse();
+    let convert =  x.toString();
+    let toArr = convert.split('');
+    let rev = toArr.reverse();
     let j = rev.join('');
-    
-    return convert === j 
-    
+
+    if (convert === rev) {
+        return true; 
+    } else {   
+        return false;
+    } 
 };
 
-console.log(isPalindrome("121"))
+console.log(isPalindrome(155))
