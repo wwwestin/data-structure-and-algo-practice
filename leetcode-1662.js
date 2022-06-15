ab8dcb195f03c23aae28817a58ae435414fdfd10
Westin Humble <westin.h@gmail.com>
2022-06-15 12:36:03 -0400

    leetcode 1662 repeat

--- leetcode-1662.js
+++ leetcode-1662.js
@@ -14,20 +14,13 @@
 // The strings are the same, so return true.
 
 const word1 = ["ab", "c"]
-const word2 = ["a", "bc"]
+const word2 = ["a", "bd"]
 
 const arrayStringsAreEqual = function(word1, word2) {
-    // declare variables to join strings
-    // conditional to check if two join words are strictly equal
-    // return boolean depending on congruency
-    
-    let w1=word1.join("")
-    let w2=word2.join("")
+    let j1 = word1.join('');
+    let j2 = word2.join('');
 
-    if(w1===w2)
-        return true;
-    return false
-
+    return j1 === j2;
 };
 
 console.log(arrayStringsAreEqual(word1, word2))
