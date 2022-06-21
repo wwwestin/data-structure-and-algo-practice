889a9a8944a03cdeaad2d89d147f2580319d255b
Westin Humble <westin.h@gmail.com>
2022-06-21 14:21:30 -0400

    leetcode 20 2nd repeat setup

--- leetcode-20.js
+++ leetcode-20.js
@@ -19,15 +19,7 @@
 
 const isValid = function(s) {
   
-  s=s.split("");
-    let stack=[];
-        for(i=0;i<s.length;i++){
-          if(s[i]=="{"||s[i]=="["||s[i]=="(") stack.push(s[i]);
-          if(s[i]=="}") if(stack[stack.length-1]=="{") stack.pop(); else return false;
-          if(s[i]=="]") if(stack[stack.length-1]=="[") stack.pop(); else return false;
-          if(s[i]==")") if(stack[stack.length-1]=="(") stack.pop(); else return false;
-        }
-    return stack.length==0;
+  
     
 };
 
