5c0adc044a5ae1881a68fff6d630613660bd050a
Westin Humble <westin.h@gmail.com>
2022-06-22 10:43:33 -0400

    leetcode 121 solved

--- leetcode-121.js
+++ leetcode-121.js
@@ -14,5 +14,21 @@
 // Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
 
 const maxProfit = function(prices) {
+    // declare variables for storing profit and left pointer at the first index of array
+    // for loop to iterate over entire length of array
+    // declare variable for right pointer that reflects current position of loop
+    // conditional for setting left pointer equal to right pointer if left is bigger than right
+    // conditional  for setting profit equal to right pointer minus left pointer if right pointer minus left pointer is greater than current profit
+    // return profit
     
+    let profit = 0,left = prices[0]
+
+    for(let i = 1; i < prices.length ; i++){
+        let right = prices[i];
+        if(left > right) left = right
+        if(right - left > profit) profit = right - left
+    }
+    return profit;
 };
+
+console.log(maxProfit([7,1,5,3,6,4]))
