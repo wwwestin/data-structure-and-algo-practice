d42d6d51bdc7fa49ff4eb886df6ce58e8f05ac88
Westin Humble <westin.h@gmail.com>
2022-06-22 10:08:16 -0400

    leetcode 21 solved

--- leetcode-21.js
+++ leetcode-21.js
@@ -4,6 +4,12 @@
 
 // Return the head of the merged linked list.
 
-var mergeTwoLists = function(list1, list2) {
+const list1 = [1,2,4];
+const list2 = [1,3,4];
 
+const mergeTwoLists = function(list1, list2) {
+   
+    return list1.concat(list2).sort();
 };
+
+console.log(mergeTwoLists(list1, list2))
