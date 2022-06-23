00944510d3a2e404e697ee8f2b3a3ffebfc4e384
Westin Humble <westin.h@gmail.com>
2022-06-23 15:40:16 -0400

    leetcode 283 solved

--- leetcode-283.js
+++ leetcode-283.js
@@ -9,6 +9,16 @@
 // Input: nums = [0,1,0,3,12]
 // Output: [1,3,12,0,0]
 
-var moveZeroes = function(nums) {
+const moveZeroes = function(nums) {
+   let k = 0
    
+    for(let i=0; i<nums.length; i++){
+        if(nums[i] != 0){
+            [nums[i], nums[k]] = [nums[k], nums[i]]
+            k++
+        }
+    }
+    return nums;
 };
+
+console.log(moveZeroes([0,1,0,3,12]));
