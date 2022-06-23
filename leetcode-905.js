ad5cd73edf17117689446307486148fef589df10
Westin Humble <westin.h@gmail.com>
2022-06-23 16:10:48 -0400

    leetcode 905 2nd repeat

--- leetcode-905.js
+++ leetcode-905.js
@@ -18,14 +18,14 @@ const sortArrayByParity = function(nums) {
     let even = [];
     let odd = [];
 
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] % 2 === 0) {
-            even.push(nums[i])
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] % 2 === 0) {
+        even.push(nums[i]) 
     } else {
         odd.push(nums[i]);
     }
 }
-    return even.concat(odd)
+    return even.concat(odd);
 };
 
 console.log(sortArrayByParity([3,1,2,4]));
