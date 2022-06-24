11f00f16ee79654c6c91c1d1401127f62608d5fe
Westin Humble <westin.h@gmail.com>
2022-06-24 10:23:24 -0400

    leetcode 226 solved

--- leetcode-226.js
+++ leetcode-226.js
@@ -8,5 +8,14 @@
 // Output: [4,7,2,9,6,3,1]
 
 const invertTree = function(root) {
+    if(!root) return root
     
+    let temp = root.right
+    root.right = root.left, root.left = temp
+    invertTree(root.right)
+    invertTree(root.left)
+    
+    return root;
 };
+
+console.log(invertTree([4,2,7,1,3,6,9]));
