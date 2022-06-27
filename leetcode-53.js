4d0f140da3529ab3274d2ad599499cdc92aabb14
Westin Humble <westin.h@gmail.com>
2022-06-27 16:58:05 -0400

    leetcode 53 solved

--- leetcode-53.js
+++ leetcode-53.js
@@ -10,5 +10,20 @@
 
 const maxSubArray = function(nums) {
   
-   
+    let currentSum = nums[0], maxSum = nums[0];
+
+    for(let i = 1; i < nums.length; i++){
+      if(currentSum < 0){
+        currentSum = nums[i];
+      } else{
+        currentSum += nums[i]
+      }
+  
+      if(currentSum > maxSum){
+        maxSum = currentSum;
+      }
+    }
+    return maxSum;
 };
+
+console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]));
