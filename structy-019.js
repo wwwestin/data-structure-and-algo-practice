8a2bf6000d48fb81c9c6331b3915e44a6781af3a
Westin Humble <westin.h@gmail.com>
2022-06-30 17:05:55 -0400

    structy 019 solved iteratvely & recursively

--- structy-019.js
+++ structy-019.js
@@ -1,8 +1,13 @@
 // Write a function, linkedListValues, that takes in the head of a linked list as an argument. The function should return an array containing all values of the nodes 
 // in the linked list.
 
-const linkedListValues = (head) => {
-   
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
 const a = new Node("a");
 const b = new Node("b");
 const c = new Node("c");
@@ -12,6 +17,28 @@ a.next = b;
 b.next = c;
 c.next = d;
 
+const linkedListValues = (head) => {
+   
+// let current = head;
+// const values = [];
+
+// while(current !== null) {
+//     values.push(current.val)
+//     current = current.next;
+// }
+// return values;
+
+//  RECURSIVE SOLUTION: 
+
+const values = [];
+  _linkedListValues(head, values);
+  return values;
 };
 
+const _linkedListValues = (head, values) => {
+    if (head === null) return;
+    values.push(head.val);
+    _linkedListValues(head.next, values);
+  };
+  
 console.log(linkedListValues(a));
