05434986224a9aa5ff8c670e9eb2e827971b9424
Westin Humble <westin.h@gmail.com>
2022-06-30 11:37:11 -0400

    structy 013 solved linear time and space complexity

--- structy-013.js
+++ structy-013.js
@@ -1,12 +1,21 @@
 const intersection = (a, b) => {
-    let repeat = [];
+    // let repeat = [];
 
-    for(num of b) {
-        if(a.includes(num))
-        repeat.push(num)
+    // for(num of b) {
+    //     if(a.includes(num))
+    //     repeat.push(num)
+    // }
+
+    // return repeat;
+
+  const result = [];
+  const setA = new Set(a);
+  for (let item of b) {
+    if (setA.has(item)) {
+      result.push(item);
     }
-
-    return repeat;
+  }
+  return result;
   };
 
   console.log(intersection([4,2,1,6], [3,6,9,2,10]));
