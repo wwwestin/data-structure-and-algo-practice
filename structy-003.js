6c2316ff3c5c8686dd80f4c23655d0583e0c1299
Westin Humble <westin.h@gmail.com>
2022-07-18 08:54:16 -0400

    structy 003 repeat setup

--- structy-003.js
+++ structy-003.js
@@ -1,12 +1,12 @@
+// Write a function, maxValue, that takes in array of numbers as an argument. The function should return the largest number in the array.
+
+// Solve this without using any built-in array methods.
+
+// You can assume that the array is non-empty.
+
 const maxValue = (nums) => {
 
-let max = -Infinity;
-  
-  for (let num of nums) {
-    if (num > max) max = num;
-  }
-  
-  return max;
+
   
 };
   
