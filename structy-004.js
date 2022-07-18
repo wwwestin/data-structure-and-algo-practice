d511ee6b9ebdd876fe669fd5580b3b6ca08e88ec
Westin Humble <westin.h@gmail.com>
2022-07-18 09:03:43 -0400

    structy 004 repeat setup

--- structy-004.js
+++ structy-004.js
@@ -1,12 +1,9 @@
-const isPrime = (n) => {
-    if (n===1) return true;
+// Write a function, isPrime, that takes in a number as an argument. The function should 
+// return a boolean indicating whether or not the given number is prime.
 
-    for(let i = 2; i < n; i += 1) {
-        if(n % i === 0) {
-        return false
-    }    
-}
-return true;
+
+const isPrime = (n) => {
+   
 };
 
 console.log(isPrime(7));
