4df96d1b5f242bb56f95a70338fdc46b006047a6
Westin Humble <westin.h@gmail.com>
2022-07-18 09:01:26 -0400

    structy 003 repeat solved

--- structy-003.js
+++ structy-003.js
@@ -6,8 +6,12 @@
 
 const maxValue = (nums) => {
 
+let max = -Infinity;
 
-  
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] > max) max = nums[i];
+}
+  return max;
 };
   
   console.log(maxValue([4, 7, 2, 8, 10, 9]));
