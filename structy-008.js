5a0e75c8c2a840a4233b6888081cb14ddfad8712
Westin Humble <westin.h@gmail.com>
2022-07-19 11:13:19 -0400

    structy 008 repeat setup

--- structy-008.js
+++ structy-008.js
@@ -1,29 +1,10 @@
+// Write a function, anagrams, that takes in two strings as arguments. The function 
+// should return a boolean indicating whether or not the strings are anagrams. 
+// Anagrams are strings that contain the same characters, but in any order.
+
 const anagrams = (s1, s2) => {
 
-    let count = {};
     
-    for(let char of s1) {
-        if (!(char in count)) {
-            count[char] = 0;
-        }
-        count[char] += 1
-    }
-
-    for(let char of s2) {
-        if (count[char] === undefined) {
-            return false;
-        } else {
-            count[char] -= 1
-    }
-}
-
-for(let char in count) {
-    if(count[char] !== 0) {
-        return false;
-    } 
-}
-    
-return true;
 };
 
 console.log(anagrams('monkeyswrite', 'newyorktimes'));
