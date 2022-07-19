15b769d59fd304e2e4f6882cdbfdc5e59e93d37d
Westin Humble <westin.h@gmail.com>
2022-07-19 11:05:37 -0400

    structy 007 repeat solved

--- structy-007.js
+++ structy-007.js
@@ -4,7 +4,33 @@
 
 const compress = (s) => {
 
-  
+// declare output empty array variable and empty pointer variables
+// use while loop to increment j counter for every time a similar character is encountered
+// declare variable to keep track of how often a letter needs to be added to output (by subtracting indices)
+// push letter to output once if num equals 1
+// push letter to output as many times as equal to num
+// set i equal to j
+// return joined output
+
+
+  const output = [];
+  let i = 0;
+  let j = 0;
+
+  while (j <= s.length) {
+    if (s[i] === s[j]) {
+      j += 1;
+    } else {
+      const num = j - i
+      if (num === 1) {
+        output.push(s[i]);
+      } else {
+        output.push(String(num), s[i]);
+      }
+      i = j
+    }
+  }
+  return output.join('');
   };
 
   console.log(compress('ssssbbz'));
