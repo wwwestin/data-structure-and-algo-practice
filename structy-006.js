92ce6dbd7c7a771008fb191ed3cb966f41cfa050
Westin Humble <westin.h@gmail.com>
2022-07-19 10:26:53 -0400

    structy 006 repeat setup

--- structy-006.js
+++ structy-006.js
@@ -1,22 +1,13 @@
+// Write a function, uncompress, that takes in a string as an argument. The input string will be formatted into 
+// multiple groups according to the following pattern:
+
+// for example, '2c' or '3a'.
+// The function should return an uncompressed version of the string where each 'char' of a group is repeated 'number' 
+// times consecutively. You may assume that the input string is well-formed according to the previously mentioned pattern.
+
+
 const uncompress = (s) => {
-    const numbers = "0123456789";
-    let output = "";
-    let i = 0;
-    let j = 0;
     
-    while (j < s.length) {
-        if(numbers.includes(s[j])) {
-        j += 1;
-        } else {
-        const num = Number(s.slice(i, j));
-        for (let count = 0; count < num; count +=1) {
-            output += s[j];
-        } 
-        j += 1;
-        i =j;
-        }
-    }
-    return output;
 };
 
 console.log(uncompress("2c3a1t"))
