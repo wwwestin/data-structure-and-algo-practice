ddc846629193d65e05c994c14959928c634bc8f9
Westin Humble <westin.h@gmail.com>
2022-07-20 09:34:06 -0400

    structy 009 repeat setup

--- structy-009.js
+++ structy-009.js
@@ -1,21 +1,13 @@
+// Write a function, mostFrequentChar, that takes in a string as an argument. The 
+// function should return the most frequent character of the string. If there are ties, 
+// return the character that appears earlier in the string.
+
+// You can assume that the input string is non-empty.
+
+
 const mostFrequentChar = (s) => {
 
-  let count = {};
-  
-  for(let char of s) {
-    if(!(char in count)) {
-      count[char] = 0;
-    }
-    count[char] += 1;
-  }
-
-  let best = null;
-  for(let char of s) {
-    if(best === null || count[char] > count[best]) {
-      best = char;
-    }
-  }
-     return best;
+ 
 };
 
 console.log(mostFrequentChar('bookeeper'));
