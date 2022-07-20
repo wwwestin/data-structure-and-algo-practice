56004329756dfeac1254f0890d8042f122be6c5c
Westin Humble <westin.h@gmail.com>
2022-07-20 09:56:48 -0400

    structy 010 repeat setup

--- structy-010.js
+++ structy-010.js
@@ -1,30 +1,16 @@
+// Write a function, pairSum, that takes in an array and a target sum as arguments. The 
+// function should return an array containing a pair of indices whose elements sum to 
+// the given target. The indices returned must be unique.
+
+// Be sure to return the indices, not the elements themselves.
+
+// There is guaranteed to be one such pair that sums to the target.
+
+
 const pairSum = (numbers, targetSum) => {
     
-    // let result = [];
 
-    // for(let i = 0; i < numbers.length; i++) {
-    //     for (let j = i + 1; j < numbers.length; j++) {
-    //         if(numbers[i] + numbers[j] == targetSum) 
-    //         result.push(i,j);
-    //     }
-    // }
-    // return result;
-
-    let previous = {};
-
-    for(let i = 0; i < numbers.length; i += 1) {
-        const num = numbers[i];
-        const complement = targetSum - num;
-
-        if (complement in previous) {
-            return [i, previous[complement]]
-        }
-        previous[num] = i;
-    }
   };
 
   console.log(pairSum([3, 2, 5, 4, 1], 8));
-
-//   time O(n)
-//   space 0(n)
 
