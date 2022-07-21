6e896361e15bff81c0b0907f2dcd83e37b9b65e8
Westin Humble <westin.h@gmail.com>
2022-07-21 15:52:40 -0400

    structy 020 solved

--- structy-020.js
+++ structy-020.js
@@ -20,8 +20,15 @@ c.next = d;
 d.next = e;
 
 const sumList = (head) => {
-    
-  
+    let sum = 0;
+    let current = head;
+
+    while(current !== null) {
+        sum += current.val
+        current = current.next;
+    }
+
+  return sum;
 };
 
 console.log(sumList(a));
