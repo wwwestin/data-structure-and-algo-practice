79dbb9c0e1973325868d57a715658b6e27214fad
Westin Humble <westin.h@gmail.com>
2022-07-21 14:53:00 -0400

    structy 014 repeat solved

--- structy-014.js
+++ structy-014.js
@@ -7,8 +7,28 @@
 
 
 const fiveSort = (nums) => {
+    // create two pointer variables (one for start of array and one starting at the end)
+    // while loop to iterate as long as i is less than j
+    // during while loop, decrement j if element is 5
+    // else if condition for if nums of i is 5, then rearrange that 5 element to the
+    // end of the existing array, then increment i
+    // otherwise, increment i
+    // return modified original array
 
-    
+    let i = 0;
+    let j = nums.length - 1;
+
+    while(i < j) {
+        if(nums[j] === 5) {
+            j -= 1;
+        } else if (nums[i] === 5) {
+            [nums[i], nums[j]] = [nums[j], nums[i]];
+            i += 1;
+        } else {
+            i += 1;
+        }
+    }
+return nums;
 };
 
 console.log(fiveSort([12, 5, 1, 5, 12, 7]));
