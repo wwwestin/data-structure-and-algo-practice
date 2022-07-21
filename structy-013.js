674ec003a823ab941fa77f449d58ea538c7b7fb1
Westin Humble <westin.h@gmail.com>
2022-07-21 14:14:55 -0400

    structy 013 repeat setup

--- structy-013.js
+++ structy-013.js
@@ -1,24 +1,12 @@
+// Write a function, intersection, that takes in two arrays, a,b, as arguments. The 
+// function should return a new array containing elements that are in both of the 
+// two arrays.
+
+// You may assume that each input array does not contain duplicate elements.
+
 const intersection = (a, b) => {
-    // let repeat = [];
-
-    // for(num of b) {
-    //     if(a.includes(num))
-    //     repeat.push(num)
-    // }
-
-    // return repeat;
-
-  const result = [];
-  const setA = new Set(a);
-  for (let item of b) {
-    if (setA.has(item)) {
-      result.push(item);
-    }
-  }
-  return result;
+   
   };
 
-  console.log(intersection([4,2,1,6], [3,6,9,2,10]));
+  console.log(intersection([4,2,1], [1,2,4,6]));
 
-//   Time: O(n+m)
-//   Space: O(n)
