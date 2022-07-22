389002da598e9ad2413847460b94bf5c68ec4c3c
Westin Humble <westin.h@gmail.com>
2022-07-22 14:30:38 -0400

    structy 021 solved

--- structy-021.js
+++ structy-021.js
@@ -20,7 +20,22 @@ c.next = d;
 
 
 const linkedListFind = (head, target) => {
-    
+    // Iterative solution:
+
+    // let current = head;
+
+    // while(current !== null) {
+    // if(current.val == target) return true;
+    // current = current.next;
+    // }
+    // return false;
+
+
+    // Recursive solution:
+
+    if(head === null) return false;
+    if(head.val === target) return true;
+    return linkedListFind(head.next, target)
   };
 
-  console.log(linkedListFind(a, "c"));
+  console.log(linkedListFind(a, "d"));
