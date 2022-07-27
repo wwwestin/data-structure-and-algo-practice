fe15a4b1c4946d0be809cdb3995d89bbe136a0bc
Westin Humble <westin.h@gmail.com>
2022-07-27 14:48:24 -0400

    structy 030 solved

--- structy-030.js
+++ structy-030.js
@@ -2,8 +2,23 @@
 // The function should create a linked list containing each element of the array as 
 // the values of the nodes. The function should return the head of the linked list.
 
+class Node {
+    constructor(val) {
+      this.val = val;
+      this.next = null;
+    }
+  }
+
+
 const createLinkedList = (values) => {
-    
+    const dummyHead = new Node(null);
+    let tail = dummyHead;
+
+    for (let val of values) {
+        tail.next = new Node(val);
+        tail = tail.next
+    }
+    return dummyHead.next;
 };
 
 console.log(createLinkedList(["h", "e", "y"]));
