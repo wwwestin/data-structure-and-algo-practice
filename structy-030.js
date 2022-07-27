106b87c5772ef12064db98c76dbe1c2988888a8f
Westin Humble <westin.h@gmail.com>
2022-07-27 14:35:30 -0400

    structy 030 setup

---
+++ structy-030.js
@@ -0,0 +1,10 @@
+// Write a function, createLinkedList, that takes in an array of values as an argument. 
+// The function should create a linked list containing each element of the array as 
+// the values of the nodes. The function should return the head of the linked list.
+
+const createLinkedList = (values) => {
+    
+};
+
+console.log(createLinkedList(["h", "e", "y"]));
+
