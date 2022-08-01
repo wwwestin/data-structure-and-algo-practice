84daf3d270f9d0f8de0ea8db87919befec2bc729
Westin Humble <westin.h@gmail.com>
2022-08-01 16:57:58 -0400

    structy 006 repeat

--- structy-006.js
+++ structy-006.js
@@ -7,34 +7,23 @@
 
 
 const uncompress = (s) => {
-    // declare variables for number library, blank string, and coounters for two points
-    // iterate through input string with while loop
-    // use .includes to check if numbers are present in string
-    // if numbers are encountered, increment iteration by one
-    // create variable where numbers are removed from string to be used as length of loop to add letters to output
-    // use for loop to add letters number of times specified in num
-    // increment j
-    // have i catch up to j
-    // return output
-
-  const numbers = "0123456789";
-  let output = [];
+  let result = [];
+  const numbers = '0123456789';
   let i = 0;
   let j = 0;
-
   while (j < s.length) {
     if (numbers.includes(s[j])) {
-        j += 1
+      j += 1;
     } else {
-        const num = Number(s.slice(i, j));
-        for (let count = 0; count < num; count +=1) {
-            output.push(s[j]);
-          } 
-          j += 1;
-          i = j;
+      const num = Number(s.slice(i, j));
+      for (let count = 0; count < num; count += 1) {
+        result.push(s[j]);
+      }
+      j += 1;
+      i = j;
     }
   }
-  return output.join('');
+  return result.join('');
 };
 
-console.log(uncompress("2c3a1t"))
+console.log(uncompress("2c3a1t"));
