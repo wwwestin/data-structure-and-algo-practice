45c7834143b8629625faa47329183f66746a70a9
Westin Humble <westin.h@gmail.com>
2022-08-02 17:39:46 -0400

    structy 006 repeat solved

--- structy-006.js
+++ structy-006.js
@@ -7,22 +7,26 @@
 
 
 const uncompress = (s) => {
+  const numbers = "0123456789";
+  
   let result = [];
-  const numbers = '0123456789';
+
   let i = 0;
   let j = 0;
+
   while (j < s.length) {
     if (numbers.includes(s[j])) {
-      j += 1;
-    } else {
-      const num = Number(s.slice(i, j));
-      for (let count = 0; count < num; count += 1) {
-        result.push(s[j]);
-      }
+     j += 1;
+  } else {
+    const num = Number(s.slice(i, j));
+    for (let count = 0; count < num; count += 1) {
+      result.push(s[j]);
+    }
       j += 1;
       i = j;
     }
   }
+
   return result.join('');
 };
 
