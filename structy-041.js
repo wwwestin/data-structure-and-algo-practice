f5179784177f47e5a6560d287f51ce95273adb23
Westin Humble <westin.h@gmail.com>
2022-08-02 11:20:22 -0400

    structy 041 solved

--- structy-041.js
+++ structy-041.js
@@ -25,7 +25,31 @@ b.right = e;
 c.right = f;
 
 const treeMinValue = (root) => {
+
+    // Iterative depth-first solution:
+
+    // let stack = [ root ];
+
+    // let min = Infinity;
+
+    // while (stack.length > 0) {
+    //     const current = stack.pop();
+
+    //     if (current.val < min) min = current.val;
+    //     if (current.left !== null) stack.push(current.left);
+    //     if (current.right !== null) stack.push(current.right);
+        
+    // }
+
+    // return min;
+
+    // Recursive depth-first solution:
+
+    if (root === null) return Infinity;
+    const leftMin = treeMinValue(root.left);
+    const rightMin = treeMinValue(root.right);
     
+    return Math.min(root.val, leftMin, rightMin);
 };
 
 console.log(treeMinValue(a));
