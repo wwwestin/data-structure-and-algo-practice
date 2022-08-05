f2e4856f3d5b9881fd797e2f2d880258a282563a
Westin Humble <westin.h@gmail.com>
2022-08-05 14:06:52 -0400

    leetcode 9 repeat solved

--- leetcode-387.js
+++ leetcode-387.js
@@ -16,11 +16,10 @@
 
 const firstUniqChar = function(s) {
     
-    for (let i = 0; i <= s.length; i++) {
-        if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) return i;
-    } 
-        return -1
-    
+   for (let i = 0; i < s.length; i++) {
+    if (s.indexOf(s[i]) == s.lastIndexOf(s[i])) return i;
+   }
+    return -1;
 };
 
 console.log(firstUniqChar('livelaughlove'));
