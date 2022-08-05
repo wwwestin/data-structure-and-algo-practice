d344920d8a1b9fcd75d93b059a29382da68209bf
Westin Humble <westin.h@gmail.com>
2022-08-05 14:19:04 -0400

    leetcode 771 repeat solved

--- leetcode-771.js
+++ leetcode-771.js
@@ -1,4 +1,6 @@
-// You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+// You're given strings jewels representing the types of stones that are jewels, and stones representing the stones 
+// you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are 
+// also jewels.
 
 // Letters are case sensitive, so "a" is considered a different type of stone from "A".
 
@@ -16,14 +18,14 @@
 
 const numJewelsInStones = function(jewels, stones) {
     if (!jewels || !stones) return 0;
+   
+    let output = 0;
 
-    let count = 0;
-
-    for(s of stones) {
-        if (jewels.includes(s)) count++
+    for (const j of stones) {
+        if (jewels.includes(j)) output ++;
     }
 
-    return count;
+    return output;
 };
     
    
