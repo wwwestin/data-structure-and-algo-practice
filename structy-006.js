f386a3bbd9366b453e6e95a4fd2df0a70de0796c
Westin Humble <westin.h@gmail.com>
2022-08-08 11:09:15 -0400

    structy 006 repeat solved

--- structy-006.js
+++ structy-006.js
@@ -3,33 +3,34 @@
 
 // for example, '2c' or '3a'.
 // The function should return an uncompressed version of the string where each 'char' of a group is repeated 'number' 
-// times consecutively. You may assume that the input string is well-formed according to the previously mentioned pattern.
+// times consecutively. You may assume that the input string is well-formed according to the previously mentioned 
+// pattern.
 
 
 const uncompress = (s) => {
   
   const numbers = "0123456789";
 
-  let result = [];
-
   let i = 0;
   let j = 0;
 
+  let result = [];
+
   while (j < s.length) {
-    if (numbers.includes(s[j])) {
-      j += 1;
+    if (numbers.includes(s[j])) { 
+      j += 1; 
   } else {
     const num = Number(s.slice(i, j));
-    for (let count = 0; count < num; count += 1) {
+
+    for(let count = 0; count < num; count += 1) {
       result.push(s[j])
     }
     j += 1;
     i = j;
   }
 }
-  
   return result.join('');
-
 };
 
 console.log(uncompress("2c3a1t"));
+console.log(uncompress("4s2b"));
