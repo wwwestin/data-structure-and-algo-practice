0ab5ecba3977485944cd9fcbc25c0d3aab9f8a3e
Westin Humble <westin.h@gmail.com>
2022-08-08 15:58:58 -0400

    leetcode 1678 repeat solved

--- leetcode-1678.js
+++ leetcode-1678.js
@@ -1,8 +1,9 @@
-// You own a Goal Parser that can interpret a resulting command. The command consists of an alphabet of "G", "()" and/or "(al)" in some order. The Goal Parser will interpret "G" as the resulting "G", "()" as the resulting "o", and "(al)" as the resulting "al". The interpreted resultings are then concatenated in the original order.
+// You own a Goal Parser that can interpret a resulting command. The command consists 
+// of an alphabet of "G", "()" and/or "(al)" in some order. The Goal Parser will 
+// interpret "G" as the resulting "G", "()" as the resulting "o", and "(al)" as the 
+// resulting "al". The interpreted resultings are then concatenated in the original order.
 
 // Given the resulting command, return the Goal Parser's interpretation of command.
-
- 
 
 // Example 1:
 
@@ -22,31 +23,33 @@
 // Input: command = "(al)G(al)()()G"
 // Output: "alGalooG"
 
-const command = "G()(al)"
 
 const interpret = function(command) {
-    let phrase = "";
+   let result = '';
 
-    for (let i = 0; i < command.length; i++) {
+   for (let i = 0; i < command.length; i++) {
         if (command[i] == "G") {
-            phrase += "G"
-        } 
+            result += command[i]; 
+        }
 
         if (command[i] == "(") {
             if (command[i + 1] == ")") {
-            phrase += "o"
-            i += 1
+            result += "o";
+            i += 1;
+            
         } else {
-            phrase += "al"
+            result += "al"
             i += 3
-        }  
-        }  
+        }
     }
-    return phrase;
+   }
+   return result;
 };
 
 
 
-console.log(interpret(command))
+console.log(interpret("G()()()()(al)"));
+console.log(interpret("G()(al)"));
+
 
 
