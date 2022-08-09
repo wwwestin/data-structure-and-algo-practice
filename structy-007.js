a2ba98148b5f8cfee1bcb9a8fa9203cbb3f7a6ca
Westin Humble <westin.h@gmail.com>
2022-08-09 15:58:56 -0400

    structy 007 repeat solved

--- structy-007.js
+++ structy-007.js
@@ -4,27 +4,27 @@
 
 const compress = (s) => {
 
-  let i = 0;
-  let j = 0;
+ let result = "";
 
-  let result = [];
+ let i = 0;
+ let j = 0;
 
-  while (j <= s.length) {
-    if (s[i] === s[j]) {
-      j += 1;
+ while (j <= s.length) {
+  if(s[i] === s[j]) {
+    j += 1;
+ } else {
+    num = j - i;
+
+    if (num === 1) {
+      result += s[i];
     } else {
-      const num = j - i;
-
-      if (num === 1) {
-        result.push[i];
-    } else {
-      result.push(String(num), s[i]);
+      result += num + s[i];
     }
     i = j;
-  }
+ }
 }
 
-  return result.join('');
+ return result;
 };
 
   console.log(compress('ssssbbz'));
