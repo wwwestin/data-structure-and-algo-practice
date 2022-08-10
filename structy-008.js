9c6837445f4da30b6d81a35c984091dca3063ec7
Westin Humble <westin.h@gmail.com>
2022-08-10 10:29:23 -0400

    structy 008 repeat solved

--- structy-008.js
+++ structy-008.js
@@ -4,38 +4,30 @@
 
 const anagrams = (s1, s2) => {
 
-    // declare empty hashmap variable to store counts of individual letter occurances
-    // for-of loop to iterate over each character in s1 and add it to the hashmap if the current count is 0 and 
-    // increase count by 1 for each occurance
-    // second for-of loop to iterate over s2 and return false if encountered character is undefined and decrease count
-    // if it IS defined
-    // for-in loop over count to return false if the count of characters DOESN'T equal zero
-    // return true if second and third loop conditions are falsey
+let count = {};
 
-    const count = {};
+for (let char of s1) {
+  if (!(char in count)) {
+    count[char] = 0;
+  }
+  count[char] += 1;
+}
+    
+for (let char of s2) {
+  if (count[char] === undefined) {
+    return false;
+  } else {
+    count[char] -= 1;
+  }
+}
 
-    for (let char of s1) {
-        if (!(char in count)) {
-          count[char] = 0;
-        }
-        count[char] += 1;
-      }
+for (let char in count) {
+  if (count[char] !== 0) {
+    return false;
+  }
+}
 
-      for (let char of s2) {
-        if (count[char] === undefined) {
-          return false;
-        } else {
-          count[char] -= 1;
-        }
-      }
-
-      for (let char in count) {
-        if (count[char] !== 0) {
-          return false;
-        }
-      }
-
-    return true;
+return true;
 };
 
 console.log(anagrams('monkeyswrite', 'newyorktimes'));
