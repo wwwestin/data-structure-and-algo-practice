617f0ae0490b5f2c8388be9a17942484c337ca2a
Westin Humble <westin.h@gmail.com>
2022-08-11 12:05:06 -0400

    structy 006 python repeat solved

--- structy-006.py
+++ structy-006.py
@@ -9,20 +9,22 @@
 # mentioned pattern.
 
 def uncompress(s):
-  numbers = '0123456789'
+  numbers = "0123456789"
   result = []
   i = 0
   j = 0
+
   while j < len(s):
     if s[j] in numbers:
       j += 1
-    else:      
-      num = int(s[i:j])
+    else:
+      num = int(s[i:j]) 
       result.append(s[j] * num)
       j += 1
       i = j
-      
+
   return ''.join(result)
+
 
 
 print(uncompress("2c3a1t"))
