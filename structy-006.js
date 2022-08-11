5f99ae75fa8487243a4fe6a6dcebf4c0519549dd
Westin Humble <westin.h@gmail.com>
2022-08-11 11:55:25 -0400

    structy 006 repeat solved

--- structy-006.js
+++ structy-006.js
@@ -11,21 +11,19 @@ const uncompress = (s) => {
   
   const numbers = "0123456789";
 
+  let result = [];
+
   let i = 0;
   let j = 0;
 
-  let result = [];
-
   while (j < s.length) {
-      if (numbers.includes(s[j])) {
-        j += 1;
-      } else {
-        const num = Number(s.slice(i, j));
-        for (let count = 0; count < num; count +=1) {
+    if (numbers.includes(s[j])) {
+      j += 1;
+    } else {
+      const num = Number(s.slice(i, j))
 
-        result.push(s[j]);
-
-        // create second loop to push number to result derived from i and j indices
+      for (let count = 0; count < num; count += 1) {
+        result.push(s[j])
       }
       j += 1;
       i = j;
