e3c7119cc96076074bbda33a664f2d829d5b6829
Westin Humble <westin.h@gmail.com>
2022-08-12 13:02:45 -0400

    structy 014 repeat solved

--- structy-014.js
+++ structy-014.js
@@ -1,6 +1,6 @@
 // Write a function, fiveSort, that takes in an array of numbers as an argument. The function should 
-// rearrange elements of the array such that all 5s appear at the end. Your function should perform this operation 
-// in-place by mutating the original array. The function should return the array.
+// rearrange elements of the array such that all 5s appear at the end. Your function should perform this 
+// operation in-place by mutating the original array. The function should return the array.
 
 // Elements that are not 5 can appear in any order in the output, as long as all 5s are at the end of the array.
 
@@ -21,7 +21,7 @@ const fiveSort = (nums) => {
             i += 1;
         }
     }
-    return nums;
+   return nums;
 };
 
 console.log(fiveSort([12, 5, 1, 5, 12, 7]));
