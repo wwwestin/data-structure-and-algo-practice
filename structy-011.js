3689a60dacffaea30e9d457fffb8682ae828c9c2
Westin Humble <westin.h@gmail.com>
2022-08-17 11:21:42 -0400

    structy 011 repeat solved

--- structy-011.js
+++ structy-011.js
@@ -1,15 +1,23 @@
+// Write a function, pairProduct, that takes in an array and a target product as arguments. The function should 
+// return an array containing a pair of indices whose elements multiply to the given target. The indices returned 
+// must be unique.
+
+// Be sure to return the indices, not the elements themselves.
+
+// There is guaranteed to be one such pair whose product is the target.
+
 const pairProduct = (numbers, targetProduct) => {
     let previous = {};
 
-    for(let i = 0; i < numbers.length; i += 1) {
+    for (let i = 0; i < numbers.length; i += 1) {
         const num = numbers[i];
-        const complement = targetProduct / num;
+        const complement = targetProduct/num;
 
         if (complement in previous) {
             return [i, previous[complement]]
         }
         previous[num] = i;
     }
-  };
+};
 
-  console.log(pairProduct([3, 2, 5, 4, 1], 8));
+console.log(pairProduct([3, 2, 5, 4, 1], 8));
