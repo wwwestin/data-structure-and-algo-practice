69bfa9b058e5066d5d12a853ffbf35b4cf9b123a
Westin Humble <westin.h@gmail.com>
2022-08-22 10:35:07 -0400

    structy 013 repeat solved

--- structy-013.js
+++ structy-013.js
@@ -5,14 +5,15 @@
 // You may assume that each input array does not contain duplicate elements.
 
 const intersection = (a, b) => {
-  let output = [];
+ 
+  let result = [];
 
-  for (s of b) {
-    if (a.includes(s)) output.push(s)
-  }
+    for (duplicate of a) {
+      if (b.includes(duplicate)) result.push(duplicate);
+    }
 
-  return output;
+  return result;
 };
 
   console.log(intersection([4,2,1], [1,2,4,6]));
-
+  console.log(intersection([4,2,1,6], [3,6,9,2,10]));
