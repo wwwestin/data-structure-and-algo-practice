e5fa7099180ff9e84c178488b3d1bf0acd43e62b
Westin Humble <westin.h@gmail.com>
2022-09-21 10:52:55 -0400

    structy 008 repeat solved

--- structy-008.js
+++ structy-008.js
@@ -4,23 +4,22 @@
 
 const anagrams = (s1, s2) => {
 
-  let count = {};
+let count = {};
 
-  for (char of s1) {
-    if (!(char in count)) {
-      count[char] = 0;
-    }
-    count[char] += 1;
+for (char of s1) {
+  if (!(char in count)) {
+    count[char] = 0
   }
+  count[char] += 1;
+}
 
-  for (let char of s2) {
-    if (count[char] === undefined) {
-      return false;
-    } else {
-      count[char] -= 1;
-    }
+for (char of s2) {
+  if (char in count) {
+    count[char] -= 1;
   }
-  
+}
+  let sum = null;
+
   for (let char in count) {
     if (count[char] !== 0) {
       return false;
