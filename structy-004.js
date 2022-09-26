3351fc0b99f72a1f378c707d1aaed11ef8a79132
Westin Humble <westin.h@gmail.com>
2022-09-26 14:02:37 -0400

    structy 004 repeat solved

--- structy-004.js
+++ structy-004.js
@@ -5,7 +5,7 @@
 const isPrime = (n) => {
     if (n < 2) return false;
 
-    for (let i = 2; i < n; i +=1) {
+    for (let i = 2; i <= Math.sqrt(n); i++) {
       if (n % i === 0) return false;
     }
 
