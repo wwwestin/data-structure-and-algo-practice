3676ef67cc9677467835dd8cbfbef0b7d08bc5e4
Westin Humble <westin.h@gmail.com>
2022-09-27 14:21:55 -0400

    structy 008 repeat solved

--- structy-008.js
+++ structy-008.js
@@ -4,21 +4,22 @@
 
 const anagrams = (s1, s2) => {
 
-let count = {};
+  let count = {};
 
-for (char of s1) {
-  if (!(char in count)) {
-    count[char] = 0
+  for (char of s1) {
+    if (!(char in count)) {
+      count[char] = 0;
+    }
+    count[char] += 1;
   }
-  count[char] += 1;
-}
 
-for (char of s2) {
-  if (char in count) {
-    count[char] -= 1;
+  for (let char of s2) {
+    if (count[char] === undefined) {
+      return false;
+    } else {
+      count[char] -= 1;
+    }
   }
-}
-  let sum = null;
 
   for (let char in count) {
     if (count[char] !== 0) {
@@ -27,6 +28,7 @@ for (char of s2) {
   }
   
   return true;
+
 };
 
 console.log(anagrams('monkeyswrite', 'newyorktimes'));
