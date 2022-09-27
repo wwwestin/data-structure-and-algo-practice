e2aef375450b3dc6ac4039fbdbbe0946ba075c3d
Westin Humble <westin.h@gmail.com>
2022-09-27 14:03:18 -0400

    structy 007 repeat solved

--- structy-007.js
+++ structy-007.js
@@ -4,27 +4,30 @@
 
 const compress = (s) => {
 
- let result = "";
+  let i = 0;
+  let j = 0;
 
- let i = 0;
- let j = 0;
+  let result = [];
 
- while (j <= s.length) {
-  if(s[i] === s[j]) {
-    j += 1;
- } else {
-    num = j - i;
-
-    if (num === 1) {
-      result += s[i];
+  while (j <= s.length) {
+    if (s[i] === s[j]) {
+      j += 1; // if s and i pointers are the same character, advance j by one
     } else {
-      result += num + s[i];
-    }
-    i = j;
- }
-}
+      const num = j - i; // if s and i are different, store the difference between them in a variable for each iteration
 
- return result;
+      if (num === 1) {
+        result.push(s[i]); // if num is one, push current iteration to result array
+      } else {
+        result.push(String(num),s[i]) // if num is larger than one, wrap num in String to push current iteration num number of times
+      }
+      i = j;
+    } 
+    }
+
+  return result.join("");
+
 };
 
   console.log(compress('ssssbbz'));
+  console.log(compress('ccaaatsss'));
+  console.log(compress('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'));
