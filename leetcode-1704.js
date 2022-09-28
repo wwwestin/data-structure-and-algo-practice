c6fc4ff373dc8e81f5d902d2bf13a9336463453d
Westin Humble <westin.h@gmail.com>
2022-09-28 11:00:44 -0400

    leetcode 1704 repeat solved

--- leetcode-1704.js
+++ leetcode-1704.js
@@ -1,11 +1,12 @@
-// You are given a string s of even length. Split this string into two halves of equal lengths, and let a be the first half and b be the second half.
+// You are given a string s of even length. Split this string into two halves of equal lengths, and let a be the 
+// first half and b be the second half.
 
-// Two strings are alike if they have the same number of vowels ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'). Notice that s contains uppercase and lowercase letters.
+// Two strings are alike if they have the same number of vowels ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'). 
+// Notice that s contains uppercase and lowercase letters.
 
 // Return true if a and b are alike. Otherwise, return false.
 
  
-
 // Example 1:
 
 // Input: s = "book"
@@ -13,15 +14,20 @@
 // Explanation: a = "bo" and b = "ok". a has 1 vowel and b has 1 vowel. Therefore, they are alike.
 
 const halvesAreAlike = function(s) {
-    const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
-    let result = 0;
 
-    for (let i = 0; i < s.length/2; i++) {
-      if (vowels.includes(s[i])) result++
-      if (vowels.includes(s[s.length - i - 1])) result--
-    }
-    return result === 0;
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+
+  let result = 0;
+
+  for (let i = 0; i < s.length/2; i++) {
+    if (vowels.includes(s[i])) result ++;
+    if (vowels.includes(s[s.length - i - 1])) result--;
+  }
+
+  return result === 0;
+    
 };
 
 console.log(halvesAreAlike("book"));
 console.log(halvesAreAlike("textbook"));
+console.log(halvesAreAlike("awlawdhecomin"));
