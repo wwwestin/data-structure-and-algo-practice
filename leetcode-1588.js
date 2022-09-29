a3108397e242e12036a1bea5f40c23ba20e10013
Westin Humble <westin.h@gmail.com>
2022-09-29 11:01:14 -0400

    leetcode 1588 repeat solved

--- leetcode-1588.js
+++ leetcode-1588.js
@@ -21,21 +21,18 @@
 // If we add all these together we get 1 + 4 + 2 + 5 + 3 + 7 + 11 + 10 + 15 = 58
 
 const sumOddLengthSubarrays = function(arr) {
-    let sum = 0
-    
+    let sum = 0;
+
     for (let i = 0; i < arr.length; i++) {
-        // for each element arr[i], we find the possible number of sub arrays that contain arr[i]
-        // assume the startIndex and endIndex => the number of sub arrays that contains arr[i] will be startIndex * endIndex
-        // there are i+1 possible startIndex before arr[i], including i
-        // there are arr.length - i possible endIndex after arr[i], including i
-        let startIndex = i + 1; 
-        let endIndex = arr.length - i; 
-        let oddArrNumbers = Math.ceil(startIndex*endIndex/2) // as the number of odd subarrays will always be equal or greater than the number of even subsarrays, we use ceiling
-        // the result will be the accumulative sum of arr[i] * number of appearances of arr[i] in its odd sub arrays
+        let startIndex = i + 1;
+        let endIndex = arr.length - i;
+
+        let oddArrNumbers = Math.ceil(startIndex*endIndex/2);
+
         sum += arr[i]*oddArrNumbers
     }
-    
-    return sum
+
+    return sum;
 };
 
 console.log(sumOddLengthSubarrays([1,4,2,5,3]));
