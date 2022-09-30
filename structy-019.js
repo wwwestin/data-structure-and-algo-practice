a446d95c31c8ae984810d85c68133c6cda5fe499
Westin Humble <westin.h@gmail.com>
2022-09-30 16:03:13 -0400

    structy 019 repeat solved

--- structy-019.js
+++ structy-019.js
@@ -18,17 +18,34 @@ b.next = c;
 c.next = d;
 
 const linkedListValues = (head) => {
-  //Iterative:
+  // Iterative:
+  // let values = [];
 
-  let result = [];
+  // let current = head;
 
-  while (head !== null) {
-    result.push(head.val);
-    head = head.next;
-  }
-  return result;
+  // while (current !== null) {
+  //   values.push(current.val);
 
+  //   current = current.next;
+  // }
+
+  // return values;
+
+  // Recursive:
+
+  const values = [];
+
+  _linkedListValues(head, values);
+
+  return values
 };
+
+const _linkedListValues = (head, values) => {
+  if (head === null) return;
+
+  values.push(head.val);
+  _linkedListValues(head.next, values);
+}
 
 
 
