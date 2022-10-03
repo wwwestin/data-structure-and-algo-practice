5993eef7a1fa49c9fd5501cd70c1ce6a9c3c5af1
Westin Humble <westin.h@gmail.com>
2022-10-03 14:29:22 -0400

    structy 021 python solved

--- structy-021.py
+++ structy-021.py
@@ -18,7 +18,24 @@ c.next = d
 
 
 def linked_list_find(head, target):
-    return
+
+    # Iterative:
+
+    # current = head
+
+    # while current is not None:
+    #     if current.val == target:
+    #         return True
+    #     current = current.next
+
+    # return False
+
+    # Recursive:
+
+    if (head is None): return False
+    if (head.val == target): return True
+    return linked_list_find(head.next, target)
+
 
 
 print (linked_list_find(a, "c"))
