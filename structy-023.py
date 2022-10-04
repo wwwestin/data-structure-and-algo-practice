f65fafaa61135b0a0f85b548bbddc627105e286a
Westin Humble <westin.h@gmail.com>
2022-10-04 13:50:20 -0400

    structy 023 python solved

--- structy-023.py
+++ structy-023.py
@@ -7,6 +7,9 @@ class Node:
     self.val = val
     self.next = None
 
+# def __repr__(self):
+#     return "{self.val}".format(self=self)
+
 a = Node("a")
 b = Node("b")
 c = Node("c")
@@ -21,8 +24,24 @@ d.next = e
 e.next = f
 
 
-def reverse_list(head):
-    return
+
+def reverse_list(head, prev = None):
+# Iterative:
+#   prev = None
+#   current = head
+#   while current is not None:
+#     next = current.next
+#     current.next = prev
+#     prev = current
+#     current = next
+#   return prev
+
+# Recursive:
+  if head is None:
+    return prev
+  next = head.next
+  head.next = prev
+  return reverse_list(next, head)
 
 
 print (reverse_list(a))
